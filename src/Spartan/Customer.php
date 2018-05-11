6b49cc1539907f2eb92f6fa5d977cc30a21b2f0d
Griffins Oringo <oringogriffins78@gmail.com>
2018-05-11 03:26:51 +0300

    fix crucial bug

--- src/Spartan/Customer.php
+++ src/Spartan/Customer.php
@@ -4,14 +4,16 @@
 namespace Loopy\Spartan;
 
 
+use Loopy\Spartan\Cache\CacheStore;
 use Loopy\Spartan\Http\Client;
 
 class Customer extends Member
 {
-    public function __construct(array $attributes = [])
+    public function __construct(array $attributes = [], CacheStore $cache = null)
     {
         parent::__construct($attributes);
         $this->setClient(resolve(Client::class));
+        $this->setCache(new LaravelCache());
     }
 
     public static function query()
